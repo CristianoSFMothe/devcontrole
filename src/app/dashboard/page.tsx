ec59fa9fb2f7d2700ff43b381962e2343a3bf120
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-07-03 23:35:32 -0300

    🆙 feat: criação inicial da página home e dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@/components/container";
+
+const Dashboard = () => {
+  return (
+    <Container>
+      <h1 className="text-white">Página Dashboard</h1>
+    </Container>
+  );
+};
+
+export default Dashboard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-// import Image from 'next/image'
+import Image from 'next/image'
+import heroImg from "@/asserts/hero.svg"
 
 export default function Home() {
   return (
-    <main>
-     <h1>Teste</h1>
+    <main className='text-white flex items-center flex-col justify-center min-h-[calc(100vh -80px)]'>
+     <h2 className='font-medium text-2xl mb-2 subTitle'>Gerencie sua empresa</h2>
+
+     <h1 className='font-bold text-3xl mb-8 text-blue-500 md:text-4xl title'>Atendimento, clientes</h1>
+
+     <Image 
+      src={heroImg}
+      alt='Logo hero do dev controle'
+      width={600}
+      className='max-w-sm md:max-w-xl logo'
+     />
     </main>
   )
 }
